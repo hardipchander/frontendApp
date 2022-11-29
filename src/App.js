af00b900b2bb6bec6f673e3c7aefd430e526b21c
Hardip Chander <hardipchander@gmail.com>
2022-11-29 14:53:46 -0500

    I added homepage allTasks and single task but need to style them

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import {Routes, Route} from 'react-router-dom';
 
 //Import Componets/Pages here
 import Home from "./Componets/Home.js";
+import AllTasks from './Componets/AllTasks.js';
+import SingleTask from './Componets/SingleTask';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/tasks" element={<AllTasks/>}/>
+        <Route path="/:taskId" element={<SingleTask/>}/>
       </Routes>   
     </div>
   );
