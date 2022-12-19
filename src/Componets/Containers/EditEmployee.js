0486a4be2d8ef1004a06b59d2a3cb76827f73898
Hardip Chander <hardipchander@gmail.com>
2022-12-18 19:41:57 -0500

    Added feature in edit Employee Container/Page where user can add or remove tasks, need to test it more

--- src/Componets/Containers/EditEmployee.js
+++ src/Componets/Containers/EditEmployee.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {connect} from "react-redux";
 import {useParams} from "react-router-dom";
-import {Navigate} from 'react-router-dom';
+import {Navigate, Link} from 'react-router-dom';
 import '../../Styles/EditEmployee.css';
 
-import {fetchSingleEmployeeThunk, editEmployeeThunk, fetchAllTasksThunk} from '../../store/thunks';
+import {fetchSingleEmployeeThunk, editEmployeeThunk, fetchAllTasksThunk, editTaskThunk, fetchSingleTaskThunk} from '../../store/thunks';
+import { editTask } from "../../store/actions/allActionCreators";
 
 // Have this for getting url
 const withRouter = WrappedComponent => props => {
@@ -101,6 +102,11 @@ class EditEmployee extends React.Component {
     }
 
     render() {
+        // Need employee infor and all the tasks and the thunk to edit an employee
+        let { employee, allTasks, editEmployee, editTask, fetchTask} = this.props;
+        let currentTasks = allTasks.filter(task => task.employeeId===employee.id);
+        let availableTasks = allTasks.filter(task => task.employeeId===null);
+
         // Go Back 
         if(this.state.redirect) {
             return (<Navigate to={`/employees/${this.state.redirectId}`} replace={true}/>);
@@ -131,6 +137,27 @@ class EditEmployee extends React.Component {
                 </form>
                 <br/>
                 {this.state.inputError!=="" && <h2 className="input-employee-form-error">{this.state.inputError}</h2>}
+                <br/>
+                <ul className="Tasks-of-Employee">Current Tasks:
+                    <p></p> 
+                    {currentTasks.map( task => {
+                        return (
+                        <li key={task.id} className="Each-Task"><Link to={`/tasks/${task.id}`} className="task-description"><span className="task-description-text">{task.description}</span></Link> 
+                        <button onClick={async () =>{await editTask({id:task.id, employeeId: null}); fetchTask(task.id) }}>-</button></li>
+                        );
+                    })}
+                </ul>
+
+                <ul className="Tasks-For-Employee">Available Tasks:
+                    <p></p> 
+                    {availableTasks.map( task => {
+                        return (
+                        <li key={task.id} className="Each-Task"><Link to={`/tasks/${task.id}`} className="task-description"><span className="task-description-text">{task.description}</span></Link>
+                        <button onClick={async () => { await editTask({id:task.id, employeeId: employee.id}); fetchTask(task.id) }}>+</button> </li>
+                        );
+                    })}
+                </ul>
+
             </div>
         );
     }
@@ -148,7 +175,9 @@ const mapDispatch=(dispatch) => {
     return({
         editEmployee: (employee) => dispatch(editEmployeeThunk(employee)),
         fetchEmployee: (id) => dispatch(fetchSingleEmployeeThunk(id)),
-        fetchTasks: () => dispatch(fetchAllTasksThunk())
+        fetchTasks: () => dispatch(fetchAllTasksThunk()),
+        editTask: (task) => dispatch(editTaskThunk(task)),
+        fetchTask: (id) => dispatch(fetchSingleTaskThunk(id)),
 
     })
 }
